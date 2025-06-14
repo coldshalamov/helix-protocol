b70d1deaceafb9b327f39e17eb4730b1a6e9946e
coldshalamov <93robingattis@gmail.com>
2025-06-14 18:31:49 -0400

    Update genesis file and hash

--- helix/config.py
+++ helix/config.py
@@ -1,4 +1,4 @@
 """Configuration constants for the Helix protocol."""
 
-GENESIS_HASH = "81b20d124bafced4f4dc4549b9a5cb7bdc1c2a92d4dab611085d9cfc882a7c70"
+GENESIS_HASH = "f2a521ab40a1211a0a034aac27a55186ccf6163c715d7deafb55790f0e37b8c3"
 
